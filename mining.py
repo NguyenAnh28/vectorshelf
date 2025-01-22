af72cb2a96ea1d4fcebb0ae8683198202b0b498d
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2025-01-21 23:27:14 -0500

    update

--- mining.py
+++ mining.py
@@ -1,15 +1,30 @@
 import requests
 import json
 
-url = 'https://www.whichbook.net/api/v1/search?q4=8&q9=2&q2=3&q8=3&q10=3&q12=3&q1=4&pagesize=25'
+url = 'https://www.whichbook.net/api/v1/search?pagesize=499&page=1'
 response = requests.get(url)
 
 if response.status_code == 200:
     print("Data fetched successfully:")
     data = response.json()
+
+
+    filtered_data = []
+    for item in data['books']:
+        filtered_item = {   
+            "id": item.get("id"),
+            "title": item.get("title"),
+            "author": item.get("author"),
+            "review": item.get("review"),
+            "publicationYear": item.get("publicationYear"),
+            "imageUrl": item.get("imageUrl"),
+        }
+        filtered_data.append(filtered_item)
+    
+    with open('data.json', 'w') as json_file:
+        json.dump(filtered_data, json_file, indent=4)
+
 else:
     print(f"Failed. Status code: {response.status_code}")
 
-with open('dataset4.json', 'w') as json_file:
-    json.dump(data, json_file, indent=4)
 
