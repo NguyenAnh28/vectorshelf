894073cdc2f70d8cfe0f7be7faa98841648285f2
NguyenAnh28 <nguyenanh.tr2006@gmail.com>
2025-01-25 03:31:33 -0500

    Install Milvus

---
+++ milvus_standalone.py
@@ -0,0 +1,9 @@
+from pymilvus import connections, utility, MilvusException
+
+connections.connect(host="localhost", port="19530")
+
+try:
+    collections = utility.list_collections()
+    print(f"List all collections: \n", collections)
+except MilvusException as e:
+    print(e)
